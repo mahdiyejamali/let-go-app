abe1af4978b6ed9b30bd8ae420647fbaf49c6d85
mahdiyejamali <mahdiye.jamali68@gmail.com>
2020-03-22 13:30:16 -0700

    adds prettier tslint config

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import '../styles/index.css';
+import Button from '@material-ui/core/Button';
 
-class App extends React.PureComponent {
+class App extends PureComponent {
+	handleClick = () => {
+		// Open setting goal page
+	};
+
 	render() {
 		return (
-			<div>
-				<h1>Hello World!</h1>
+			<div className="text-center-h text-center-v">
+				<h1>Let's get started!</h1>
+				<Button variant="contained" color="primary" onClick={this.handleClick}>
+					Start
+				</Button>
 			</div>
 		);
 	}
